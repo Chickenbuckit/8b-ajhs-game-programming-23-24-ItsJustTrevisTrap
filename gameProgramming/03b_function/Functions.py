6f8faa131cfba8f6d776f86045e21f2f0e60c06a
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-10-23 14:51:03 -0400

    S

---
+++ gameProgramming/03b_function/Functions.py
@@ -0,0 +1,37 @@
+import random
+# FUNCTION  -- a named piece of code that can be reused easily.
+# FUNCTION SIGNATURE -- Syntax for creating a new function.
+def exampleFunction(): # NO PARAMETERS
+    count = 0
+    num = int(input("How many times do you want to wish a happy birthday?\n"))
+    while count < num:
+        print("Happy Birthday!\n")
+        count += 1
+
+def exampleFunctionB(num): # PARAMETERS
+    while count < num:
+        print("Happy Birthday!\n")
+        count += 1
+
+# IF A FUNCTION REQUIRES PARAMETERS, YOUR CODE WILL CRASH WITHOUT THEM!
+
+# RUNNING A FUNCTION IS KNOWN AS CALLING THE FUNCTION!
+# exampleFunctionA()
+# exampleFunctionB(5, 0)
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
+
+rollDice(3, 6)
+rollDice(1, 20)
+
+stregth
+
+
