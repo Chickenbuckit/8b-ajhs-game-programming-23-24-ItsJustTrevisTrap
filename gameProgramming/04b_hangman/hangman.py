cebd3beaf637bc8566fc4f84f107aa75575a59d1
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-10-31 13:57:56 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,1 +1,11 @@
 # Hangman Game by Trevis Brown, v0.0
+
+words = 'Acid Knife Bones Bleach Reptile Banshee Spirit Spider Snake Disease Skull Photosynthesis Mental Flatline Havoc Yamaha Glove Lime Green Teal Eyes Teeth Cell Blood Phobia Venus Saturn Kawasaki Kill Wendigo'.split()
+print(words)
+
+
+
+
+
+
+
