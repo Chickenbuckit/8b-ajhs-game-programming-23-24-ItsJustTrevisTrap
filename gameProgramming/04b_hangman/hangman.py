33e26853a049cb700f24447af1a87f71ffdb358e
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-10-31 14:34:48 -0400

    yes
    oiuygu

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,11 +1,54 @@
-# Hangman Game by Trevis Brown, v0.0
-
+# Hangman Game by Trevis Brown, v0.1
+import random
 words = 'Acid Knife Bones Bleach Reptile Banshee Spirit Spider Snake Disease Skull Photosynthesis Mental Flatline Havoc Yamaha Glove Lime Green Teal Eyes Teeth Cell Blood Phobia Venus Saturn Kawasaki Kill Wendigo'.split()
-print(words)
 
+#VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+    +---+
+        |    
+        |
+        |    
+    =========''','''
+    +---+
+    O   |    
+        |
+        |    
+    =========''','''
+    +---+
+    O   |    
+    |   |
+        |    
+    =========''','''
+    +---+
+    O   |    
+   /|   |
+        |    
+    =========''','''
+    +---+
+    O   |    
+   /|\  |
+        |    
+    =========''','''
+    +---+
+    O   |    
+   /|\  |
+   /    |    
+    =========''','''
+    +---+
+    O   |    
+   /|\  |
+   / \  |    
+    =========''']
 
+# Pick Word from List
+def getRandomWord(wordlist): # Return a random word from the list.
+    wordIndex = random.randint(0, len(wordList) - 1)
+    # len(listName) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
+    return wordList[wordIndex]
 
-
+i = 0
+while i < 50:
+    word = getRandomWord(words)
 
 
 
