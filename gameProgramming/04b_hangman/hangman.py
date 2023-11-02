e09fdbaa80ca32ee8fcf3f129c18f9b48a8a541c
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-11-02 13:45:56 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -53,6 +53,18 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     print('Missed Letters:', end = ' ')
     for eachLetter in missedLetters:
         print(eachLetter, end = ' ')
+    print()
+
+    blanks = '_' * len(secretWord)
+    
+    # Replace Blanks with Correct letters
+    for i in range(len(secretWord)):
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks:
+        print(letter, end = ' ')
+    print()             
 
 # i = 0
 # while i < 50:
