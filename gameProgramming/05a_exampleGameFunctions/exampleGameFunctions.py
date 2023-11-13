ff5f6c6c933fcddd1d969f8bf149e0b821cb4a84
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-11-13 14:46:35 -0500

    Update exampleGameFunctions.py

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,24 @@
 # Example Game Functions Project, Trevis Brown, v0.0
+import random
+
+
+
+
+
+
+
+
+
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
+      
