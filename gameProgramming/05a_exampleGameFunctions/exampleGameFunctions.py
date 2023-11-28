414d8af34d3587b47bd75cd093de42dbd3cb78e4
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-11-28 13:57:40 -0500

    v0.0

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,25 +1,13 @@
 # Example Game Functions Project, Trevis Brown, v0.0
+import pygame
 import random
-# words = ['Engine Wheels Rims Spoiler Pedals GearShift Windows Truck Battery AirFilter OilFilter TailLight Seat Wipers Gaskets SparkPlugs Fender Bumper Suspension DiskBreakv ]
-# DICTIONARY VERSION
-# Stored in key: Value Pairs
-# Actual Dictionary Word (Key) : Value (Definition)
 
+# Initialize pygame
+pygame.init()
 
+# Set up the game window
+width = 800
+height = 600
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Racing Game")
 
-
-
-
-
-def functionOne():
-    pass
-
-def functionTwo(param1):
-    pass
-
-def functionThree(param1 = "Default Value"):
-    pass
-
-def functionFour(param1, param2, param3):
-    pass
-      
